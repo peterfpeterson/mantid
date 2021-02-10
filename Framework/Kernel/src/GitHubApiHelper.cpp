d44ccb168b426f752499f73de3306511141efceb
Pete Peterson <petersonpf@ornl.gov>
2021-02-10 17:26:24 -0500

    Allow for specifying API key in ConfigService
    ConfigService wins over whatever was supplied at compile time

--- Framework/Kernel/src/GitHubApiHelper.cpp
+++ Framework/Kernel/src/GitHubApiHelper.cpp
@@ -4,6 +4,7 @@
 //   NScD Oak Ridge National Laboratory, European Spallation Source,
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
+#include "MantidKernel/ConfigService.h"
 #include "MantidKernel/GitHubApiHelper.h"
 #include "MantidKernel/ConfigService.h"
 #include "MantidKernel/DateAndTime.h"
